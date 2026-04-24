9d3bf117821f3e3323ba0112377961c1a8e4fdec
Luca Foppiano <luca@foppiano.org>
2026-04-24 14:21:49 +0100

    chore: spotless

--- src/test/java/org/commoncrawl/net/WarcUriTest.java
+++ src/test/java/org/commoncrawl/net/WarcUriTest.java
@@ -7,35 +7,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WarcUriTest {
 
-    @Test
-    void getHostName_malformedHttps_shouldNotBeEmpty(){
-        WarcUri warcUri = new WarcUri("https:////www.google.com/robot.txt");
-        assertNotNull(warcUri.getHostName());
-        assertTrue(StringUtils.isNotEmpty(warcUri.getHostName().getHostName()), "getHostName() should not return an empty string.");
-        assertEquals("www.google.com", warcUri.getHostName().getHostName(), "getHostName() should return 'www.google.com' for the malformed URL.");
-    }
+	@Test
+	void getHostName_malformedHttps_shouldNotBeEmpty() {
+		WarcUri warcUri = new WarcUri("https:////www.google.com/robot.txt");
+		assertNotNull(warcUri.getHostName());
+		assertTrue(
+				StringUtils.isNotEmpty(warcUri.getHostName().getHostName()),
+				"getHostName() should not return an empty string.");
+		assertEquals(
+				"www.google.com",
+				warcUri.getHostName().getHostName(),
+				"getHostName() should return 'www.google.com' for the malformed URL.");
+	}
 
-    @Test
-    void getHostName_malformedHttp_shouldNotBeEmpty(){
-        WarcUri warcUri = new WarcUri("http:////www.google.com/robot.txt");
-        assertNotNull(warcUri.getHostName());
-        assertTrue(StringUtils.isNotEmpty(warcUri.getHostName().getHostName()), "getHostName() should not return an empty string.");
-        assertEquals("www.google.com", warcUri.getHostName().getHostName(), "getHostName() should return 'www.google.com' for the malformed URL.");
-    }
+	@Test
+	void getHostName_malformedHttp_shouldNotBeEmpty() {
+		WarcUri warcUri = new WarcUri("http:////www.google.com/robot.txt");
+		assertNotNull(warcUri.getHostName());
+		assertTrue(
+				StringUtils.isNotEmpty(warcUri.getHostName().getHostName()),
+				"getHostName() should not return an empty string.");
+		assertEquals(
+				"www.google.com",
+				warcUri.getHostName().getHostName(),
+				"getHostName() should return 'www.google.com' for the malformed URL.");
+	}
 
-    @Test
-    void getHostName_validHttpHost_shouldNotBeEmpty(){
-        WarcUri warcUri = new WarcUri("http://sites.google.com////robot.txt");
-        assertNotNull(warcUri.getHostName());
-        assertTrue(StringUtils.isNotEmpty(warcUri.getHostName().getHostName()), "getHostName() should not return an empty string.");
-        assertEquals("sites.google.com", warcUri.getHostName().getHostName(), "getHostName() should return 'sites.google.com' for the URL with extra path slashes.");
-    }
+	@Test
+	void getHostName_validHttpHost_shouldNotBeEmpty() {
+		WarcUri warcUri = new WarcUri("http://sites.google.com////robot.txt");
+		assertNotNull(warcUri.getHostName());
+		assertTrue(
+				StringUtils.isNotEmpty(warcUri.getHostName().getHostName()),
+				"getHostName() should not return an empty string.");
+		assertEquals(
+				"sites.google.com",
+				warcUri.getHostName().getHostName(),
+				"getHostName() should return 'sites.google.com' for the URL with extra path slashes.");
+	}
 
-    @Test
-    void getHostName_validHttpsHost_shouldNotBeEmpty(){
-        WarcUri warcUri = new WarcUri("https://sites.google.com////robot.txt");
-        assertNotNull(warcUri.getHostName());
-        assertTrue(StringUtils.isNotEmpty(warcUri.getHostName().getHostName()), "getHostName() should not return an empty string.");
-        assertEquals("sites.google.com", warcUri.getHostName().getHostName(), "getHostName() should return 'sites.google.com' for the URL with extra path slashes.");
-    }
+	@Test
+	void getHostName_validHttpsHost_shouldNotBeEmpty() {
+		WarcUri warcUri = new WarcUri("https://sites.google.com////robot.txt");
+		assertNotNull(warcUri.getHostName());
+		assertTrue(
+				StringUtils.isNotEmpty(warcUri.getHostName().getHostName()),
+				"getHostName() should not return an empty string.");
+		assertEquals(
+				"sites.google.com",
+				warcUri.getHostName().getHostName(),
+				"getHostName() should return 'sites.google.com' for the URL with extra path slashes.");
+	}
 }
